bc3811939bed2909f1463074660e614805d993fe
SashaPolyanski <Sashapolyanski@gmail.com>
2023-12-22 11:27:47 +0300

    fixed path

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,6 @@ import i18n from 'i18next'
 import {initReactI18next} from 'react-i18next'
 
 import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
 
 const ns = [
   'common',
@@ -10,7 +9,6 @@ const ns = [
 ]
 i18n
   .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'ru',
