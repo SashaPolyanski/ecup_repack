4c7faf1cb30ff4ae93fa513d07235f5fbb4edb02
SashaPolyanski <Sashapolyanski@gmail.com>
2024-01-02 18:11:57 +0300

    fixed overflow content

--- src/view/Tournament/TournamentBracket/TournamentsUsersTable.tsx
+++ src/view/Tournament/TournamentBracket/TournamentsUsersTable.tsx
@@ -6,7 +6,6 @@ import { TabsComponents } from "./TournamentModalContent";
 export const TournamentsUsersTable: FC<TabsComponents> = ({ teams }) => {
   return (
     <Box sx={{ overflow: "auto", height: "90%" }}>
-      {" "}
       {teams?.map((m, i) => (
         <Box display={"flex"} mt={2} alignItems={"center"}>
           <Box mr={2}>{i + 1}</Box> <TeamInfo team={m} />
